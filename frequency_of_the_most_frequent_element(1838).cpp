349017e8f9034de89c673cdaa4e382b6ce377517
satvik maheshwari <72884440+sarcasvik@users.noreply.github.com>
2023-08-03 14:07:10 +0000

    1838. Frequency of the Most Frequent Element

---
+++ frequency_of_the_most_frequent_element(1838).cpp
@@ -0,0 +1,22 @@
+// sliding window problem
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, long k) {
+        long sum = 0 , ans = 1; 
+        sort(nums.begin(),nums.end());
+        long l =0, r= 0;
+        while(r<nums.size()){
+            sum = sum + nums[r];
+            while(nums[r]*(r-l+1) > sum +k){
+                sum = sum - nums[l];
+                l++;
+            } 
+            ans = max(ans, r-l+1);
+            r++;
+        }
+        
+        return ans;
+    }
+};
+
+//while(nums[r]*(r-l+1) <= sum +k) is the condition we need to follow
